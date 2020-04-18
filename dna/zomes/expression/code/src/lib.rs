ee056f3b99537f282fea208d08b1e728be93e9bc
jdeepee <joshuadparkin@gmail.com>
2020-04-19 00:07:12 +0100

    first pass at create public expression

--- dna/zomes/expression/code/src/lib.rs
+++ dna/zomes/expression/code/src/lib.rs
@@ -10,57 +10,51 @@ extern crate holochain_json_derive;
 
 pub mod methods;
 
-use hdk::{
-    entry_definition::ValidatingEntryType,
-    holochain_persistence_api::cas::content::Address
-};
 use hdk::holochain_core_types::{
-    entry::Entry,
-    dna::entry_types::Sharing,
-    agent::AgentId,
-    chain_header::ChainHeader
+    chain_header::ChainHeader, dna::entry_types::Sharing, entry::Entry,
+};
+use hdk::{
+    entry_definition::ValidatingEntryType, error::ZomeApiResult,
+    holochain_persistence_api::cas::content::Address, AGENT_ADDRESS,
 };
 
-use hdk::holochain_json_api::{
-    error::JsonError,
-    json::JsonString,
-};
+use hdk::holochain_json_api::{error::JsonError, json::JsonString};
 use hdk_proc_macros::zome;
 
-/// An interface into a DNA which contains Expression information. Expected to be interacted with using expression Addresses 
-/// retrieved from a social context or by using a Identity retreived from a users social graph. 
+/// An interface into a DNA which contains Expression information. Expected to be interacted with using expression Addresses
+/// retrieved from a social context or by using a Identity retreived from a users social graph.
 /// In this situation you can see that the Expression DNA/trait does not need to include any index capability
 /// as this is already infered to the agent by the place they got the expression from; social context or social graph.
 ///
 /// If the expression should be private to a group of people then the host DNA should be membraned.
 pub trait ExpressionDao {
-    /// Create an expression and link it to yourself publicly with optional dna_address pointing to 
+    /// Create an expression and link it to yourself publicly with optional dna_address pointing to
     /// dna that should ideally be used for linking any comments to this expression
-    fn create_public_expression(content: String, inter_dna_link_dna: Option<Address>) -> Expression;
+    fn create_public_expression(content: String) -> ZomeApiResult<Expression>;
     /// Get expressions authored by a given Agent/Identity
     fn get_by_author(author: Identity, count: u32, page: u32) -> Vec<Expression>;
     fn get_expression_by_address(address: Address) -> Option<Expression>;
-    
+
     /// Send an expression to someone privately p2p
     fn send_private(to: Identity, content: String, inter_dna_link_dna: Option<Address>);
     /// Get private expressions sent to you
     fn inbox() -> Vec<Expression>;
 }
 
-pub type Identity = AgentId;
+pub type Identity = AGENT_ADDRESS;
 
-#[derive(Serialize, Deserialize, Debug, DefaultJson,Clone)]
+#[derive(Serialize, Deserialize, Debug, DefaultJson, Clone)]
 pub struct ShortFormExpression {
     background: Vec<String>,
-    body: String
+    body: String,
 }
 
 /// A holochain expression
+#[derive(Serialize, Deserialize, Debug, DefaultJson, Clone)]
 struct Expression {
     entry: Entry,
     headers: Vec<ChainHeader>,
     expression_dna: Address,
-    inter_dna_link_dna: Option<Address>,
 }
 
 #[zome]
@@ -74,7 +68,7 @@ pub mod shortform_expression {
             validation_package: || {
                 hdk::ValidationPackageDefinition::Entry
             },
-    
+
             validation: | _validation_data: hdk::EntryValidationData<ShortFormExpression>| {
                 Ok(())
             }
@@ -89,5 +83,10 @@ pub mod shortform_expression {
     #[validate_agent]
     pub fn validate_agent(validation_data: EntryValidationData<AgentId>) {
         Ok(())
+    }
+
+    #[zome_fn("expression")]
+    pub fn create_public_expression(content: String) -> ZomeApiResult<Expression> {
+        Expression::create_public_expression(content)
     }
 }
--- dna/zomes/expression/code/src/methods.rs
+++ dna/zomes/expression/code/src/methods.rs
@@ -1,30 +1,55 @@
 use hdk::{
-    holochain_persistence_api::cas::content::Address
+    error::{ZomeApiError, ZomeApiResult},
+    holochain_core_types::entry::Entry,
+    holochain_persistence_api::cas::content::Address,
+    holochain_persistence_api::hash::HashString,
+    prelude::{GetEntryOptions, GetEntryResultType, StatusRequestKind},
+    AGENT_ADDRESS, DNA_ADDRESS,
 };
 
-use crate::{Expression, ExpressionDao, Identity};
+use crate::{Expression, ExpressionDao, Identity, ShortFormExpression};
 
 impl ExpressionDao for Expression {
-    /// Create an expression and link it to yourself publicly with optional dna_address pointing to 
+    /// Create an expression and link it to yourself publicly with optional dna_address pointing to
     /// dna that should ideally be used for linking any comments to this expression
-    fn create_public_expression(content: String, inter_dna_link_dna: Option<Address>) -> Expression {
+    fn create_public_expression(content: String) -> ZomeApiResult<Expression> {
+        // Serialize data to check its valid and prepare for entry into source chain
+        let expression: ShortFormExpression = serde_json::from_str(&content)
+            .map_err(|err| ZomeApiError::Internal(err.to_string()))?;
+        let expression_entry = Entry::App("expression".into(), expression.into());
 
+        // Commit and link entry
+        let expression_address = hdk::commit_entry(&expression_entry)?;
+        hdk::link_entries(&AGENT_ADDRESS, &expression_address, "", "")?;
+
+        // Get headers for commited entry - is there a better way to do this @Nico?
+        let entries_headers = match hdk::get_entry_result(
+            &expression_address,
+            GetEntryOptions {
+                status_request: StatusRequestKind::default(),
+                entry: false,
+                headers: true,
+                timeout: Default::default(),
+            },
+        )?
+        .result
+        {
+            GetEntryResultType::Single(result) => result.headers,
+            _ => vec![],
+        };
+
+        Ok(Expression {
+            entry: expression_entry,
+            headers: entries_headers,
+            expression_dna: HashString::from(DNA_ADDRESS.to_string()),
+        })
     }
+
     /// Get expressions authored by a given Agent/Identity
-    fn get_by_author(author: Identity, count: u32, page: u32) -> Vec<Expression> {
-
-    }
-
-    fn get_expression_by_address(address: Address) -> Option<Expression> {
-
-    }
-    
+    fn get_by_author(author: Identity, count: u32, page: u32) -> Vec<Expression> {}
+    fn get_expression_by_address(address: Address) -> Option<Expression> {}
     /// Send an expression to someone privately p2p
-    fn send_private(to: Identity, content: String, inter_dna_link_dna: Option<Address>){
-
-    }
+    fn send_private(to: Identity, content: String, inter_dna_link_dna: Option<Address>) {}
     /// Get private expressions sent to you
-    fn inbox() -> Vec<Expression>{
-
-    }
+    fn inbox() -> Vec<Expression> {}
 }
