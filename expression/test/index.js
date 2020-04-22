e84c16e875c501a865de66e0ae79481e4ed2a078
Nicolas Luck <nicolas@lucksus.eu>
2020-04-22 19:55:43 +0200

    Fix test,  use actually declared variables

--- expression/test/index.js
+++ expression/test/index.js
@@ -55,7 +55,7 @@ orchestrator.registerScenario("test create and get public expression", async (s,
   // indicating the function, and passing it an input
   const result  = await alice.call("ShortFormExpression", "expression", "create_public_expression", {content : JSON.stringify({background: ["bg1", "bg2"], body: "Test ShortForm Expression"})})
   t.deepEqual(result.hasOwnProperty("Ok"), true)
-  await s.consistency() 
+  await s.consistency()
 
   const get = await bob.call("ShortFormExpression", "expression", "get_by_author", {"author": alice.instance('ShortFormExpression').agentAddress, page_size: 10, page_number: 0})
   t.deepEqual(get.hasOwnProperty("Ok"), true)
@@ -64,15 +64,15 @@ orchestrator.registerScenario("test create and get public expression", async (s,
 
 orchestrator.registerScenario("test send and receive private", async (s, t) => {
   const {alice, bob} = await s.players({alice: conductorConfig, bob: conductorConfig}, true)
-  await s.consistency() 
+  await s.consistency()
 
   const send = await alice.call("ShortFormExpression", "expression", "send_private", {to: bob.instance('ShortFormExpression').agentAddress, content : JSON.stringify({background: ["bg1", "bg2"], body: "Test Private P2P ShortForm Expression"})})
-  t.deepEqual(result.hasOwnProperty("Ok"), true)
-  await s.consistency() 
+  t.deepEqual(send.hasOwnProperty("Ok"), true)
+  await s.consistency()
 
   const get_inbox = await bob.call("ShortFormExpression", "expression", "inbox", {from: null, page_size: 10, page_number: 0})
-  t.deepEqual(get.hasOwnProperty("Ok"), true)
-  t.deepEqual(get.Ok.length, 1)
+  t.deepEqual(get_inbox.hasOwnProperty("Ok"), true)
+  t.deepEqual(get_inbox.Ok.length, 1)
 })
 
 orchestrator.run()
