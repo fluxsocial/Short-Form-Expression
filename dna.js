46740f43b4e774e02845aca8138e479effe6ec55
jdeepee <joshuadparkin@gmail.com>
2021-09-15 18:37:03 +0100

    fix dna read location for language

--- dna.js
+++ dna.js
@@ -1,4 +1,4 @@
-import dna from './shortform.dna'
+import dna from './hc-dna/workdir/shortform-expression.dna'
 
 export const DNA = Buffer.from(dna, 'base64')
 export const DNA_NICK = "junto-shortform"
